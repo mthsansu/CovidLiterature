fa9f59f1540ffa839f29c40f6b99130a79684f6e
ChabiMax <57496354+ChabiMax@users.noreply.github.com>
2022-03-18 15:32:05 +0100

    Update Test.R

--- Bachotage/Test.R
+++ Bachotage/Test.R
@@ -30,14 +30,58 @@ base_an <- base_an %>% left_join(base_insider, by =c("Author.s..ID" = "Id"))
 
 base_jour_aut <- base_jour_aut %>% left_join(base_an, by =c("Author.s..ID" = "Author.s..ID"))
 
-
 base_jour_aut$insider_2020 <- 0
 base_jour_aut[base_jour_aut$Annee_entree.y < 2020, "insider_2020"] <- 1
+base_jour_aut$insider_2019 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2019, "insider_2019"] <- 1
+base_jour_aut$insider_2018 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2018, "insider_2018"] <- 1
+base_jour_aut$insider_2017 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2017, "insider_2017"] <- 1
+base_jour_aut$insider_2016 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2016, "insider_2016"] <- 1
+base_jour_aut$insider_2015 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2015, "insider_2015"] <- 1
+base_jour_aut$insider_2014 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2014, "insider_2014"] <- 1
+base_jour_aut$insider_2013 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2013, "insider_2013"] <- 1
+base_jour_aut$insider_2012 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2012, "insider_2012"] <- 1
 base_jour_aut$insider_2011 <- 0
-base_jour_aut[base_jour_aut$Annee_entree.y < 2013, "insider_2011"] <- 1
+base_jour_aut[base_jour_aut$Annee_entree.y < 2011, "insider_2011"] <- 1
+base_jour_aut$insider_2010 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2010, "insider_2010"] <- 1
+base_jour_aut$insider_2009 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2009, "insider_2009"] <- 1
+base_jour_aut$insider_2008 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2008, "insider_2008"] <- 1
+base_jour_aut$insider_2007 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2007, "insider_2007"] <- 1
+base_jour_aut$insider_2006 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2006, "insider_2006"] <- 1
+base_jour_aut$insider_2005 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2005, "insider_2005"] <- 1
+base_jour_aut$insider_2004 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2004, "insider_2004"] <- 1
+base_jour_aut$insider_2003 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2003, "insider_2003"] <- 1
+base_jour_aut$insider_2002 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2002, "insider_2002"] <- 1
 base_jour_aut$insider_2001 <- 0
-base_jour_aut[base_jour_aut$Annee_entree.y < 2003, "insider_2001"] <- 1
-
+base_jour_aut[base_jour_aut$Annee_entree.y < 2001, "insider_2001"] <- 1
+base_jour_aut$insider_2000 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 2000, "insider_2000"] <- 1
+base_jour_aut$insider_1999 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 1999, "insider_1999"] <- 1
+base_jour_aut$insider_1998 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 1998, "insider_1998"] <- 1
+base_jour_aut$insider_1997 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 1997, "insider_1997"] <- 1
+base_jour_aut$insider_1996 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 1996, "insider_1996"] <- 1
+base_jour_aut$insider_1995 <- 0
+base_jour_aut[base_jour_aut$Annee_entree.y < 1995, "insider_1995"] <- 1
 
 base_jour_aut[is.na(base_jour_aut$Cited.by),"Cited.by"] <- 0
 
