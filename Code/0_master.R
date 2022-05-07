a451bba795e3b238b82d662856fbccaa68c9d0c2
Mathis Sansu <89588443+mthsansu@users.noreply.github.com>
2022-05-07 09:07:27 +0200

    Add files via upload
    Maj Master

--- Code/0_master.R
+++ Code/0_master.R
@@ -9,7 +9,7 @@
 # 1 = Maxime
 # 2 = Mathis
 
-user_indiv <- 1
+user_indiv <- 2
 
 if (user_indiv == 1) {
   path_code <- "C:\\Users\\Dell\\Desktop\\DSSS propre\\Code"
@@ -19,10 +19,12 @@ if (user_indiv == 1) {
   path_graphs <- "XXXXX"
   path_tab <- "XXXXX"
 } else if (user_indiv == 2){
-  path_code <- "C:\\Users\\mthsa\\Desktop\\SQD_DSSS\\ProjDSSS\\Code"
-  path_data <- "C:\\Users\\mthsa\\Desktop\\SQD_DSSS\\ProjDSSS\\Data"
-  path_graphs <- "C:\\Users\\mthsa\\Desktop\\SQD_DSSS\\ProjDSSS\\Figures"
-  path_tab <- "C:\\Users\\mthsa\\Desktop\\SQD_DSSS\\ProjDSSS\\Tables"
+  path_code <- "C:\\Users\\mthsa\\Desktop\\CovidLiterature\\Code"
+  path_queries <- "C:\\Users\\mthsa\\Desktop\\CovidLiterature\\Queries"
+  path_data <- "C:\\Users\\mthsa\\Desktop\\CovidLiterature\\Data"
+  path_data_scraping <- "C:\\Users\\mthsa\\Desktop\\CovidLiterature\\Data scraping"
+  path_graphs <- "XXXXX"
+  path_tab <- "XXXXX"
 }
 
 ##### Scripts
@@ -30,10 +32,14 @@ if (user_indiv == 1) {
 setwd(path_code)
 
 #source(paste0(path_code,"\\1_requirements.R"))
-#source(paste0(path_code,"\\2_bases.R"))
-#source(paste0(path_code,"\\3_stat_des_insiders.R"))
-#source(paste0(path_code,"\\4_stat_des_journaux.R"))
-
+#source(paste0(path_code,"\\2_generation_queries_corona.R"))
+#source(paste0(path_code,"\\3_traitement_base_coronavirus.R"))
+#source(paste0(path_code,"\\4_df_auteurs.R"))
+#source(paste0(path_code,"\\5_generation_queries_auteurs.R"))
+#source(paste0(path_code,"\\6_traitement_base_auteurs.R"))
+#source(paste0(path_code,"\\7_df_auteurs_complete.R"))
+#source(paste0(path_code,"\\8_construction_capitaux.R"))
+#source(paste0(path_code,"\\9_Régressions.R"))
 
 
 
