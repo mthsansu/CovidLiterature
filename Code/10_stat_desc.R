74d1805291aebc6ae554bacb547ccb1208c258af
Mathis Sansu <89588443+mthsansu@users.noreply.github.com>
2022-05-09 22:27:50 +0200

    maj stat desc

--- Code/10_stat_desc.R
+++ Code/10_stat_desc.R
@@ -11,24 +11,8 @@ library(data.table)
 setwd(path_data)
 data_auteurs = read.csv("df_auteurs_variables2.csv")
 
-# Nombre de primo-auteurs par année (corona)
 
-data_plot <- data_auteurs %>% count(premiere_annee_contribution_coronavirus,author_id)
-data_plot$n <- NULL
-data_plot <- data_plot %>% count(premiere_annee_contribution_coronavirus)
-ggplot(data = data_plot, aes(x=premiere_annee_contribution_coronavirus)) +
-  geom_point(aes(y=n), alpha = 0.4, size = 2) +
-  geom_line(aes(y=n), size = 0.8) +
-  geom_point(aes(y=log(n)*400), colour = "darkblue", alpha = 0.4, size = 2) +
-  geom_line(aes(y=log(n)*400), colour = "darkblue", size = 0.8) +
-  geom_vline(xintercept = 2003, linetype = "dashed", color = "red") +
-  geom_vline(xintercept = 2013, linetype = "dashed", color = "red") +
-  geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
-  scale_y_continuous(name = "Nombre de primo-auteurs", sec.axis = sec_axis(~(./400), name="échelle logarithmique")) +
-  xlab("") +
-  theme_hc()
-
-# Ratio de primo-auteurs par année (corona)
+# Nombre d'auteurs et primo-auteurs par année
 
 data_plot <- data_auteurs %>% count(premiere_annee_contribution_coronavirus,author_id)
 data_plot$n <- NULL
@@ -39,7 +23,26 @@ setnames(data_plot2, names(data_plot2), c("annee","nb_aut"))
 data_plot <- data_plot %>% full_join(data_plot2)
 data_plot$ratio_primo <- data_plot$nb_primo / data_plot$nb_aut
 
-ggplot(data = data_plot, aes(x=annee)) +
+ggplot(data = data_plot[data_plot$annee < 2022,], aes(x=annee)) +
+  geom_point(aes(y=nb_aut), alpha = 0.4, size = 2) +
+  geom_line(aes(y=nb_aut), size = 0.8) +
+  geom_point(aes(y=nb_primo), alpha = 0.4, size = 2, color = "darkblue") +
+  geom_line(aes(y=nb_primo), size = 0.8, color = "darkblue") +
+  geom_point(aes(y=log(nb_aut)*500), alpha = 0.4, size = 1.5) +
+  geom_line(aes(y=log(nb_aut)*500), linetype = "dashed", size = 0.8) +
+  geom_point(aes(y=log(nb_primo)*500), colour = "darkblue", alpha = 0.4, size = 1.5) +
+  geom_line(aes(y=log(nb_primo)*500), linetype = "dashed", colour = "darkblue", size = 0.8) +
+  geom_vline(xintercept = 2003, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = 2013, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
+  scale_y_continuous(name = "Nombre d'auteurs", sec.axis = sec_axis(~(./500), name="échelle logarithmique")) +
+  xlab("") +
+  theme_hc()
+
+
+# Ratio de primo-auteurs par année (corona)
+
+ggplot(data = data_plot[data_plot$annee < 2022,], aes(x=annee)) +
   geom_point(aes(y=ratio_primo), alpha = 0.4, size = 2) +
   geom_line(aes(y=ratio_primo), size = 0.8) +
   geom_vline(xintercept = 2003, linetype = "dashed", color = "red") +
@@ -47,28 +50,6 @@ ggplot(data = data_plot, aes(x=annee)) +
   geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
   xlab("") + ylab("Ratio de primo-auteurs") +
   theme_hc()
-
-
-
-# df <- data_auteurs[data_auteurs$nb_contribs_coronavirus > 0,c("nb_articles_cumule_coronavirus","annee")]
-# data_plot <- data.frame(annee = c(1990:2022), q0 =rep(NA, 33), q25 = rep(NA, 33), q50 = rep(NA,33), q75 = rep(NA,33),
-#                         q100 = rep(NA,33), moy = rep(NA,33))
-# for (i in 1990:2022) {
-#   df_an <- df[df$annee == i,]
-#   for (j in c(1:5)) {
-#     data_plot[data_plot$annee == i, j+1] <- quantile(df_an$nb_articles_cumule_coronavirus, probs = seq(0, 1, 0.25))[j]
-#   }
-#   data_plot[data_plot$annee == i, "moy"] <- mean(df_an$nb_articles_cumule_coronavirus)
-# }
-# 
-# ggplot(data = data_plot, aes(x=annee)) +
-#   geom_line(aes(y=q25), size = 0.8) + geom_line(aes(y=q50), size = 0.8) +
-#   geom_line(aes(y=q75), size = 0.8) + geom_line(aes(y=moy), size = 0.8, color = "darkblue") +
-#   geom_vline(xintercept = 2003, linetype = "dashed", color = "red") +
-#   geom_vline(xintercept = 2013, linetype = "dashed", color = "red") +
-#   geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
-#   xlab("") + ylab("") +
-#   theme_hc()
 
 
 # Nombre moyen de contributions par auteur par année
@@ -81,7 +62,7 @@ for (i in 1990:2022) {
   data_plot[data_plot$annee == i, "moy_non_cor"] <- mean(df_an$nb_contribs)
   data_plot[data_plot$annee == i, "moy_tot"] <- data_plot[data_plot$annee == i, "moy_cor"] + data_plot[data_plot$annee == i, "moy_non_cor"]
 }
-ggplot(data = data_plot, aes(x=annee)) +
+ggplot(data = data_plot[data_plot$annee < 2022,], aes(x=annee)) +
   geom_point(aes(y=moy_cor), alpha = 0.4, size = 2, color ="red") +
   geom_line(aes(y=moy_cor), size = 0.8, color ="red") +
   geom_point(aes(y=moy_non_cor), alpha = 0.4, size = 2, color ="darkblue") +
@@ -93,5 +74,79 @@ ggplot(data = data_plot, aes(x=annee)) +
   geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
   xlab("") + ylab("Nombre moyen de contributions par auteur") +
   theme_hc()
+
+
+# Nombre moyen de citations en 2022 par auteur par année
+
+df <- data_auteurs[,c("nb_citations_coronavirus","nb_citations","annee")]
+df[is.na(df$nb_citations_coronavirus) == TRUE,"nb_citations_coronavirus"] <- 0
+df[is.na(df$nb_citations) == TRUE,"nb_citations"] <- 0
+data_plot <- data.frame(annee = c(1990:2022), moy_cor = rep(NA,33), moy_non_cor = rep(NA,33), moy_tot = rep(NA,33))
+for (i in 1990:2022) {
+  df_an <- df[df$annee == i,]
+  data_plot[data_plot$annee == i, "moy_cor"] <- mean(df_an$nb_citations_coronavirus)
+  data_plot[data_plot$annee == i, "moy_non_cor"] <- mean(df_an$nb_citations)
+  data_plot[data_plot$annee == i, "moy_tot"] <- data_plot[data_plot$annee == i, "moy_cor"] + data_plot[data_plot$annee == i, "moy_non_cor"]
+}
+ggplot(data = data_plot[data_plot$annee < 2022,], aes(x=annee)) +
+  geom_point(aes(y=moy_cor), alpha = 0.4, size = 2, color ="red") +
+  geom_line(aes(y=moy_cor), size = 0.8, color ="red") +
+  geom_point(aes(y=moy_non_cor), alpha = 0.4, size = 2, color ="darkblue") +
+  geom_line(aes(y=moy_non_cor), size = 0.8, color = "darkblue") +
+  geom_point(aes(y=moy_tot), alpha = 0.4, size = 2) +
+  geom_line(aes(y=moy_tot), size = 0.8) +
+  geom_vline(xintercept = 2003, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = 2013, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
+  xlab("") + ylab("Nombre moyen de citations par auteur") +
+  theme_hc()
+
+
+# Nombre de co-auteurs par auteur par année
+
+df <- data_auteurs[,c("nb_coauteurs_coronavirus","nb_coauteurs","annee")]
+df[is.na(df$nb_coauteurs_coronavirus) == TRUE,"nb_coauteurs_coronavirus"] <- 0
+df[is.na(df$nb_coauteurs) == TRUE,"nb_coauteurs"] <- 0
+data_plot <- data.frame(annee = c(1990:2022), moy_cor = rep(NA,33), moy_non_cor = rep(NA,33), moy_tot = rep(NA,33))
+for (i in 1990:2022) {
+  df_an <- df[df$annee == i,]
+  data_plot[data_plot$annee == i, "moy_cor"] <- mean(df_an$nb_coauteurs_coronavirus)
+  data_plot[data_plot$annee == i, "moy_non_cor"] <- mean(df_an$nb_coauteurs)
+  data_plot[data_plot$annee == i, "moy_tot"] <- data_plot[data_plot$annee == i, "moy_cor"] + data_plot[data_plot$annee == i, "moy_non_cor"]
+}
+ggplot(data = data_plot[data_plot$annee < 2022,], aes(x=annee)) +
+  geom_point(aes(y=moy_cor), alpha = 0.4, size = 2, color ="red") +
+  geom_line(aes(y=moy_cor), size = 0.8, color ="red") +
+  geom_point(aes(y=moy_non_cor), alpha = 0.4, size = 2, color ="darkblue") +
+  geom_line(aes(y=moy_non_cor), size = 0.8, color = "darkblue") +
+  geom_point(aes(y=moy_tot), alpha = 0.4, size = 2) +
+  geom_line(aes(y=moy_tot), size = 0.8) +
+  geom_vline(xintercept = 2003, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = 2013, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = 2020, linetype = "dashed", color = "red") +
+  xlab("") + ylab("Nombre moyen de co-auteurs par auteur") +
+  theme_hc()
+
+
+
+data_plot <- data_auteurs[, c("author_id","nb_citations_cumule", "nb_citations_cumule_coronavirus","annee",
+                              "derniere_annee_contribution", "nb_articles_cumule_coronavirus","nb_articles_cumule")]
+data_plot <- data_plot[data_plot$annee == data_plot$derniere_annee_contribution,]
+data_plot$nb_articles_cumule_tot <- data_plot$nb_articles_cumule + data_plot$nb_articles_cumule_coronavirus
+# data_plot[is.na(data_plot$nb_citations_cumule) == TRUE, "nb_citations_cumule"] <- 0
+# data_plot[is.na(data_plot$nb_citations_cumule_coronavirus) == TRUE, "nb_citations_cumule_coronavirus"] <- 0
+data_plot$nb_citations_cumule <- data_plot$nb_citations_cumule + 1
+data_plot$nb_citations_cumule_coronavirus <- data_plot$nb_citations_cumule_coronavirus + 1
+
+ggplot(data = data_plot, aes(x=log(nb_citations_cumule))) +
+  geom_point(aes(y=log(nb_citations_cumule_coronavirus), color = derniere_annee_contribution, size = log(nb_articles_cumule_tot)),
+             alpha = 0.8) +
+  geom_smooth(aes(y=log(nb_citations_cumule_coronavirus)), method = "lm") +
+  xlab("Citations cumulées hors coronavirus (log)") + ylab("Citations cumulées coronavirus (log)") +
+  labs(color = "Dernière année de contribution", size = "Nombre d'articles cumulés (log)") +
+  scale_color_viridis_c(option = "magma") +
+  theme_hc()
+
+
 
 
